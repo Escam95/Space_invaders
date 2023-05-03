9a8e4fcd71247de4c4cd96b2b236151d5ccb994d
24junek.t <24junek.tobias@gymjh.cz>
2023-05-03 16:18:58 +0200

    import

--- Constants.py
+++ Constants.py
@@ -4,14 +4,15 @@ import os
 import pygame.image
 
 GAME_ICON = pygame.image.load('Images/spaceShips_007.png')
-WIDTH, HEIGHT = 800, 600
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+WIDTH, HEIGHT = 1200, 800
+SCREEN_OFFSET = 75
 SPACE = (60, 63, 65)
+BULLET_COLOR = (255, 255, 255)
 FPS = 60
 VEL = 5
 BULLET_VEL = 9
+BULLET_WIDTH = 5
+BULLET_HEIGHT = 12
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 80, 60
 SPACESHIP_IMAGE = pg.image.load(os.path.join('Images', 'spaceShips_007.png'))
 SPACESHIP = pg.transform.rotate(pg.transform.scale(SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 180)
--- Space invaders.py
+++ Space invaders.py
@@ -1,20 +1,26 @@
 import pygame
 import pygame as pg
 import Constants as c
+pygame.init()
 
 clock = pg.time.Clock()
 window = pg.display.set_mode((c.WIDTH, c.HEIGHT))
 pg.display.set_caption('Space Invaders')
 pg.display.set_icon(c.GAME_ICON)
+bullets = []
 game_running = True
 
 
 def on_key_down(event):
-    global space_ship_vel
-    if event.key == pg.K_d and space_ship.x < c.WIDTH - c.SPACESHIP_WIDTH - 20:
+    global space_ship_vel, bullets
+    if event.key == pg.K_d and space_ship.x < c.WIDTH - c.SPACESHIP_WIDTH - c.SCREEN_OFFSET:
         space_ship_vel = 1
-    elif event.key == pg.K_a and space_ship.x > 20:
+    elif event.key == pg.K_a and space_ship.x > c.SCREEN_OFFSET:
         space_ship_vel = -1
+    elif event.key == pg.K_SPACE:
+        bullet = pygame.Rect(space_ship.x + c.SPACESHIP_WIDTH // 2 - c.BULLET_WIDTH // 2,
+                             space_ship.y + c.SPACESHIP_HEIGHT // 3, c.BULLET_WIDTH, c.BULLET_HEIGHT)
+        bullets.append(bullet)
 
 
 def on_key_up(event):
@@ -37,12 +43,19 @@ def game_input():
 
 
 def game_update():
+    global space_ship_vel
     space_ship.x = space_ship.x + space_ship_vel * c.VEL
+    if space_ship.x > (c.WIDTH - c.SPACESHIP_WIDTH - c.SCREEN_OFFSET) or space_ship.x < c.SCREEN_OFFSET:
+        space_ship_vel = 0
+    for bullet in bullets:
+        bullet.y -= c.BULLET_VEL
 
 
 def game_output():
     window.fill(c.SPACE)
     window.blit(c.SPACESHIP, (space_ship.x, space_ship.y))
+    for bullet in bullets:
+        pygame.draw.rect(window, c.BULLET_COLOR, bullet)
     pg.display.flip()
 
 
