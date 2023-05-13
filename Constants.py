0f6b142c580fb077ee94910d51346395c011f310
Tobiáš_Junek <125470074+Tobias-Junek@users.noreply.github.com>
2023-05-13 09:27:52 +0200

    new stuff

--- Constants.py
+++ Constants.py
@@ -1,22 +1,23 @@
 import pygame as pg
 import os
-import pygame.image
 
 images = [
+    (0, 0),
+    (4, 4),
     (5, 4)
 ]
 
 
-def get_image(sheet, width, height, image_id, scale, colour):
+def get_image(sheet, width, height, image_id, scale, colour, rotation):
     image = pg.Surface((width, height))
     image.blit(sheet, (0, 0), (images[image_id][0] * IMAGE_SIZE, images[image_id][1] * IMAGE_SIZE, width, height))
-    image = pg.transform.rotate(pg.transform.scale(image, (width * scale, height * scale)), 180)
+    image = pg.transform.rotate(pg.transform.scale(image, (width * scale, height * scale)), rotation)
     image.set_colorkey(colour)
 
     return image
 
 
-GAME_ICON = pygame.image.load('Images/spaceShips_007.png')
+GAME_ICON = pg.image.load('Images/spaceShips_007.png')
 WIDTH, HEIGHT = 1200, 800
 SCREEN_OFFSET = 75
 SPACE = (60, 63, 65)
@@ -29,8 +30,7 @@ BULLET_WIDTH = 5
 BULLET_HEIGHT = 12
 IMAGE_SIZE = 64
 tile_sheet_image = pg.image.load(os.path.join('Images', 'simpleSpace_tilesheet.png'))
-BASIC_ENEMY_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 0, 1, BLACK)
+BASIC_ENEMY_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 1, 1, BLACK, 180)
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 80, 60
-SPACESHIP_IMAGE = pg.image.load(os.path.join('Images', 'spaceShips_007.png'))
-SPACESHIP = pg.transform.rotate(pg.transform.scale(SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 180)
+SPACESHIP_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 0, 1, BLACK, 0)
 
--- Space invaders.py
+++ Space invaders.py
@@ -1,18 +1,21 @@
-import pygame
 import pygame as pg
 import Constants as c
 
-pygame.init()
+pg.init()
 
 clock = pg.time.Clock()
 window = pg.display.set_mode((c.WIDTH, c.HEIGHT))
 pg.display.set_caption('Space Invaders')
 pg.display.set_icon(c.GAME_ICON)
 bullets = []
+enemies = []
 game_running = True
 shooting_delay = 500
-ally_shot_event = pygame.USEREVENT + 1
-pygame.time.set_timer(ally_shot_event, shooting_delay)
+spawning_delay = 2000
+ally_shot_event = pg.USEREVENT + 1
+pg.time.set_timer(ally_shot_event, shooting_delay)
+spawn_basic_enemy = pg.USEREVENT + 2
+pg.time.set_timer(spawn_basic_enemy, spawning_delay)
 
 
 def on_key_down(event):
@@ -41,15 +44,19 @@ def game_input():
         elif event.type == pg.KEYUP:
             on_key_up(event)
         if event.type == ally_shot_event:
-            bullet = pygame.Rect(space_ship.x + c.SPACESHIP_WIDTH // 2 - c.BULLET_WIDTH // 2,
-                                 space_ship.y + c.SPACESHIP_HEIGHT // 3, c.BULLET_WIDTH, c.BULLET_HEIGHT)
+            bullet = pg.Rect(space_ship.x + c.IMAGE_SIZE // 2 - c.BULLET_WIDTH // 2,
+                             space_ship.y + c.IMAGE_SIZE // 4, c.BULLET_WIDTH, c.BULLET_HEIGHT)
             bullets.append(bullet)
+        if event.type == spawn_basic_enemy:
+            enemy = pg.Rect((c.WIDTH - c.IMAGE_SIZE) // 2, c.HEIGHT - c.HEIGHT // 8,
+                            c.IMAGE_SIZE, c.IMAGE_SIZE)
+            enemies.append(enemy)
 
 
 def game_update():
     global space_ship_vel
     space_ship.x = space_ship.x + space_ship_vel * c.VEL
-    if space_ship.x > (c.WIDTH - c.SPACESHIP_WIDTH - c.SCREEN_OFFSET) or space_ship.x < c.SCREEN_OFFSET:
+    if space_ship.x > (c.WIDTH - c.IMAGE_SIZE - c.SCREEN_OFFSET) or space_ship.x < c.SCREEN_OFFSET:
         space_ship_vel = 0
     for bullet in bullets:
         bullet.y -= c.BULLET_VEL
@@ -57,15 +64,16 @@ def game_update():
 
 def game_output():
     window.fill(c.SPACE)
-    window.blit(c.SPACESHIP, (space_ship.x, space_ship.y))
+    window.blit(c.SPACESHIP_IMAGE, (space_ship.x, space_ship.y))
     for bullet in bullets:
-        pygame.draw.rect(window, c.BULLET_COLOR, bullet)
-    window.blit(c.BASIC_ENEMY_IMAGE, (0, 0))
+        pg.draw.rect(window, c.BULLET_COLOR, bullet)
+    for enemy in enemies:
+        window.blit(c.BASIC_ENEMY_IMAGE, enemy)
     pg.display.flip()
 
 
-space_ship = pygame.Rect((c.WIDTH - c.SPACESHIP_WIDTH) // 2, c.HEIGHT - c.HEIGHT // 8,
-                         c.SPACESHIP_WIDTH, c.SPACESHIP_HEIGHT)
+space_ship = pg.Rect((c.WIDTH - c.IMAGE_SIZE) // 2, c.HEIGHT - c.HEIGHT // 8,
+                     c.IMAGE_SIZE, c.IMAGE_SIZE)
 space_ship_vel = 0
 while game_running:
     clock.tick(c.FPS)
