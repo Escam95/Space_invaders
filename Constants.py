f14099c2dd9b194133a8685cfcadcbf8cf2f2f10
Escam95 <125470074+Escam95@users.noreply.github.com>
2023-06-01 18:06:31 +0200

    making the code more readable

--- Constants.py
+++ Constants.py
@@ -3,6 +3,7 @@ import os
 
 pg.init()
 
+# coordinates of the images in the tilesheet
 images = [
     (0, 0),
     (7, 4),
@@ -15,6 +16,8 @@ images = [
 ]
 
 
+# a function for getting an image out of the tilesheet - you need to input the parameters of your image and add its
+# position in the images list above. Please add them below the others.
 def get_image(sheet, width, height, image_id, scale, colour, rotation):
     image = pg.Surface((width, height))
     image.blit(sheet, (0, 0), (images[image_id][0] * IMAGE_SIZE, images[image_id][1] * IMAGE_SIZE, width, height))
@@ -28,8 +31,7 @@ score = 0
 score_increment = 1
 score_a = 1
 
-upgrade_images = 4
-
+# basic constants
 GAME_ICON = pg.image.load('Images/spaceShips_007.png')
 WIDTH, HEIGHT = 1400, 800
 SCREEN_OFFSET = 75
@@ -45,14 +47,17 @@ BULLET_WIDTH = 5
 BULLET_HEIGHT = 12
 ENEMY_Y_VEL = 2
 IMAGE_SIZE = 64
+STARTING_HEALTH = 500
 tile_sheet_image = pg.image.load(os.path.join('Images', 'simpleSpace_tilesheet.png'))
 BASIC_ENEMY_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 1, 1, BLACK, 180)
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 80, 60
 SPACESHIP_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 0, 1, BLACK, 0)
 UPGRADE_ICON_IMAGE = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, 3, 1, BLACK, 0)
+
+# creating a list of the upgrade images for the allied ship
 ALLY_UPGRADES = []
-for upgrade_image_id in range(0, upgrade_images):
+UPGRADE_IMAGES = 4
+for upgrade_image_id in range(0, UPGRADE_IMAGES):
     upgrade_image = get_image(tile_sheet_image, IMAGE_SIZE, IMAGE_SIZE, upgrade_image_id + 4, 1, BLACK, 0)
     ALLY_UPGRADES.append(upgrade_image)
 
-STARTING_HEALTH = 500
--- Space invaders.py
+++ Space invaders.py
@@ -14,9 +14,12 @@ import random
 pg.init()
 
 clock = pg.time.Clock()
+#  window display
 window = pg.display.set_mode((c.WIDTH, c.HEIGHT))
 pg.display.set_caption('Space Invaders')
 pg.display.set_icon(c.GAME_ICON)
+
+#  basic variables
 bullets = []
 enemy_bullets = []
 enemies = []
@@ -34,6 +37,7 @@ health = c.STARTING_HEALTH
 ally_upgrade = 0
 space_ship_image = c.SPACESHIP_IMAGE
 
+# score text
 font_obj = pg.font.Font(None, 32)
 score_Surface = font_obj.render(str(c.score), True, (97, 222, 42), None)
 score_Rect = score_Surface.get_rect()
@@ -58,6 +62,7 @@ def on_key_up(event):
         spawn_swarm()
 
 
+#  spawning an enemy
 def spawn_basic(position):
     enemy = pg.Rect(position, 0,
                     c.IMAGE_SIZE, c.IMAGE_SIZE)
@@ -69,6 +74,7 @@ def spawn_swarm():
         spawn_basic(i * 64 + c.SCREEN_OFFSET)
 
 
+#  spawning an upgrade
 def spawn_upgrade(position):
     upgrade = pg.Rect(position, 0, c.IMAGE_SIZE, c.IMAGE_SIZE)
     upgrades.append(upgrade)
@@ -94,6 +100,7 @@ def game_update():
     if health <= 0:
         game_running = False
 
+    #  timers
     score_delay_countdown -= 1
     if score_delay_countdown == 0:
         score_Surface = font_obj.render(str(c.score), True, (97, 222, 42), None)
@@ -117,18 +124,21 @@ def game_update():
         spawn_upgrade(random.randint(c.SCREEN_OFFSET, c.WIDTH - c.IMAGE_SIZE - c.SCREEN_OFFSET))
         spawning_upgrade_countdown = spawning_upgrade_delay
 
+    #  updating the allied ship position
     space_ship.x = space_ship.x + space_ship_vel * c.VEL
 
     if space_ship.x > (c.WIDTH - c.IMAGE_SIZE - c.SCREEN_OFFSET) or space_ship.x < c.SCREEN_OFFSET:
         space_ship_vel = 0
 
+    #  updating the lists
     for upgrade in upgrades:
         global ally_upgrade, space_ship_image
         upgrade.y += c.ENEMY_Y_VEL
         if upgrade.colliderect(space_ship):
             ally_upgrade += 1
             upgrades.remove(upgrade)
-            if ally_upgrade < c.upgrade_images:
+            health += 200
+            if ally_upgrade < c.UPGRADE_IMAGES:
                 space_ship_image = c.ALLY_UPGRADES[ally_upgrade]
 
     for enemy in enemies:
@@ -158,9 +168,17 @@ def game_update():
 def game_output():
     global space_ship_image
     window.fill(c.SPACE)
+
+    #  drawing the allied ship
     window.blit(space_ship_image, (space_ship.x, space_ship.y))
+
+    #  drawing the score
     window.blit(score_Surface, score_Rect)
+
+    #  healthbar
     pg.draw.rect(window, c.ENEMY_BULLET_COLOR, (0, 0, 500, 10))
+
+    #  drawing the lists of objects
     for bullet in bullets:
         pg.draw.rect(window, c.BULLET_COLOR, bullet)
     for enemy_bullet in enemy_bullets:
@@ -177,12 +195,16 @@ space_ship = pg.Rect((c.WIDTH - c.IMAGE_SIZE) // 2, c.HEIGHT - c.HEIGHT // 8,
 space_ship_mask = pg.mask.from_surface(space_ship_image)
 space_ship_mask_image = space_ship_mask.to_surface()
 space_ship_vel = 0
+
+#  main game loop
 while game_running:
     clock.tick(c.FPS)
     game_input()
     game_update()
     game_output()
 game_running = True
+
+#  temporary exit screen
 while game_running:
     game_input()
     pg.draw.rect(window, c.BLACK, (c.WIDTH // 2, c.HEIGHT // 2, 200, 100))
